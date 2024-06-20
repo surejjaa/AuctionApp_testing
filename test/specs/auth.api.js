980f93943b937c23ff36faca88cda8bc790e9c9a
Surejja Alibegovic <surejja.a18@gmail.com>
2024-06-20 15:55:18 +0200

    Tests added

---
+++ test/specs/auth.api.js
@@ -0,0 +1,18 @@
+import requestsPage from '../pageobjects/requests.page.js'
+import dataGenerator from '../pageobjects/data.generator.js';
+
+describe('API auth test for Auction App', () => { 
+    it('makes a POST request for registration', async () => {
+        const url = await requestsPage.buildUrl('auth/register');
+        const data = await dataGenerator.register();
+        const response = await requestsPage.postRequest(url, data);
+        await requestsPage.matchingData(response.status, requestsPage.status);
+    });
+
+    it('makes a POST request for login', async () => {
+        const url = await requestsPage.buildUrl('auth/login');
+        const data = await dataGenerator.login();
+        const response = await requestsPage.postRequest(url, data);
+        await requestsPage.matchingData(response.status, requestsPage.status);
+    });
+})
